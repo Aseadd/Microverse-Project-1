5c3cec8e5d7a1d5a4e364abba8a385e0bf576ad0
addis <betsega23@gmail.com>
2022-07-19 01:32:36 +0300

    Adding JS file to make the hamburger icon responsive to an event

---
+++ validation.js
@@ -0,0 +1,16 @@
+const hamb = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu-item');
+const headline = document.querySelector('.headline');
+function myFunction() {
+  hamb.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  headline.style.top = '200px';
+}
+hamb.addEventListener('click', myFunction);
+const navItem = document.querySelectorAll('.nav-link');
+function closeMenu() {
+  hamb.classList.remove('active');
+  navMenu.classList.remove('active');
+  headline.style.top = '92px';
+}
+navItem.forEach((n) => n.addEventListener('click', closeMenu));
