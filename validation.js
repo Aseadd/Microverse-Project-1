34b302eb373d43ff6a5e0f05e031f939d876f2fc
addis <betsega23@gmail.com>
2022-07-19 03:56:01 +0300

    commit eslintr changes

--- validation.js
+++ validation.js
@@ -4,12 +4,9 @@ const headline = document.querySelector('.headline');
 function myFunction() {
   hamb.classList.toggle('active');
   navMenu.classList.toggle('active');
-  headline.style.top = '92px';
+  headline.style.top = '200px';
 }
 hamb.addEventListener('click', myFunction);
 const navItem = document.querySelectorAll('.nav-link');
-function closeMenu() {
-  hamb.classList.remove('active');
-  navMenu.classList.remove('active');
-}
+
 navItem.forEach((n) => n.addEventListener('click', closeMenu));
